fa3691e5bb76779140ea6d96395db47f26b42c1f
Natalie Tay <natalie.tay@gmail.com>
2016-06-21 15:20:47 +0800

    Render the Navbar component in the index page
    - Use `react-dom` to wire React to the DOM
    - Specify a container on the index page for the component to be
    rendered into

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const cats = ['tomtom', 'steaky', 'zorro'];
-const me = 'nat'
-console.log(`${me} loves ${cats}`);
+import React from 'react';
+import { render } from 'react-dom';
+import Navbar from './components/navbar';
+const container = document.getElementById('container');
+
+render(
+    <Navbar />, container
+);
