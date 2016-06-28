7656ef6651df926c6c8aafd3d0b6dde0416a96ff
Natalie Tay <natalie.tay@gmail.com>
2016-06-28 18:01:40 +0800

    Load styles into the app
    No simple feat, this is the bare minimum needed for the react-toolbox.
    Might want to do a version that's just for hand-writted css..(but why?)
    
    - use react-toolbox (it's great, vetted by js coach and me)
    - style-loader needs to go with css-loader, sass-loader, postcss-loader
    and node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: './index',
-    output: {
-        path: `${__dirname}/public`,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                presets: ['es2015', 'react']
-            }
-        }]
-    }
+	entry: './index',
+	output: {
+		path: `${__dirname}/public`,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			},
+			{
+				test: /(\.scss|\.css)$/,
+				loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('bundle.css', {allChunks: true})
+	]
 };
